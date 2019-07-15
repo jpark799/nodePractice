56c60b6c2dd39bd60f56f9b80b00fc5b57a7d2b2
Jason Park <jason.j.park79@gmail.com>
2019-07-14 23:12:16 -0400

    basic server

--- index.js
+++ index.js
@@ -1,7 +1,77 @@
-const Logger = require('./logger')
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+    // if(req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, { 'Content-Type': 'text/html'});
+    //         res.end('<h1>Home</h1>')
+    //     })
+    // }
 
-logger.on('message', (data) => console.log(`Called Listener:`, data));
+    // if(req.url === '/api/users') {
+    //     const users = [
+    //         { name: 'Jason Park', age: 26},
+    //         { name: 'Dahae Kim', age: 24},
+    //     ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(users));
+    // }
 
-logger.log('Hello World')
+    let filePath = path.join(__dirname, 'public', req.url === '/' ?
+    'index.html' : req.url
+    );
+
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+    switch(extname) {
+        case '.js': 
+            contentType = 'text/javascript';
+            break;
+        case '.css': 
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png': 
+            contentType = 'image/png';
+            break;
+        case '.jpg': 
+            contentType = 'image/jpg';
+            break;
+    }
+
+    // Read File
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if(err.code == 'EN0ENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(content, 'utf8');
+                })
+            } else {
+                // Server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`)
+            } 
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType});
+            res.end(content,'utf8');
+        }
+    })
+});
+
+const PORT = process.env.PORT || 5000
+
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
